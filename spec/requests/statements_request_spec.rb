b46587db60a3e378d7322db8386f0a9eacc5cddd
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-01 13:49:44 -0800

    split up some statements specs

--- spec/requests/statements_request_spec.rb
+++ spec/requests/statements_request_spec.rb
@@ -25,32 +25,72 @@ RSpec.describe 'Statement Requests', type: :request do
   end
 
   describe 'GET new' do
-    it 'renders new template' do
+    before do
       get "/accounts/#{account.id}/statements/new"
+    end
+
+    it 'renders new template' do
       expect(response).to render_template(:new)
+    end
+
+    it 'assigns unsaved @statement' do
       expect(assigns(:statement)).to_not eq(nil)
+    end
+
+    it 'returns 200 status' do
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'POST create' do
-    let(:valid_params) { { statement: { balance: 200.34, date: '1/1/2020' } } }
-    let(:invalid_params) { { statement: { balance: 'abc', date: '1/1/2020' } } }
-    
-    it 'redirects to account_path if statement is created' do
-      post "/accounts/#{account.id}/statements", params: valid_params
-      expect(response).to redirect_to(account_path(account))
-      follow_redirect!
-      expect(response).to render_template('accounts/show')
-      expect(response.body).to include('Statement created')
-      expect(response).to have_http_status(:ok)
+    context 'when parameters are valid' do
+      let(:valid_params) { { statement: { balance: 200.34, date: '1/1/2020' } } }
+      before do
+        post "/accounts/#{account.id}/statements", params: valid_params
+      end
+
+      it 'redirects to account_path of statement' do
+        expect(response).to redirect_to(account_path(account))
+      end
+
+      it 'renders accounts/show template' do
+        follow_redirect!
+        expect(response).to render_template('accounts/show')
+      end
+
+      it 'includes "Statement created" in response body' do
+        follow_redirect!
+        expect(response.body).to include('Statement created')
+      end
+
+      it 'returns 302 status before redirect' do
+        expect(response).to have_http_status(:found)
+      end
+
+      it 'returns 200 status after redirect' do
+        follow_redirect!
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'renders new template if statement is not created' do
-      post "/accounts/#{account.id}/statements", params: invalid_params
-      expect(response).to render_template(:new)
-      expect(response.body).to include('Balance is not a number')
-      expect(response).to have_http_status(:ok)
+    context 'when parameters are invalid' do
+      let(:invalid_params) { { statement: { balance: 'abc', date: '1/1/2020' } } }
+      before do
+        post "/accounts/#{account.id}/statements", params: invalid_params
+      end
+
+      it 'renders new template' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'includes "Balance is not a number" in response body' do
+
+        expect(response.body).to include('Balance is not a number')
+      end
+
+      it 'returns 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
