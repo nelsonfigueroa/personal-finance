4d0528466c88897df19a27b7cc79c18c44f99195
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-02 21:17:59 -0800

    added some scopes

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,4 +6,6 @@ class Account < ApplicationRecord
 
   validates :user, :name, presence: true
   validates :name, length: { maximum: 40 }, format: { with: /\A[a-zA-Z ]+\z/ } # only letters
+
+  scope :sorted_by_name, -> { order(name: :asc) }
 end
--- app/models/statement.rb
+++ app/models/statement.rb
@@ -6,4 +6,7 @@ class Statement < ApplicationRecord
   validates :balance, :date, presence: true
   validates :balance, numericality: { greater_than_or_equal_to: 0,
                                       less_than_or_equal_to: BigDecimal(10**8) }
+
+  scope :sorted_by_date, -> { order(date: :desc) }
+  scope :sorted_by_balance, -> { order(balance: :desc) }
 end
