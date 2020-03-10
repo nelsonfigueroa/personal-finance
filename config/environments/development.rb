fb9ad7f065729fb3f10828d1219d72bd41be3f7f
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-09 19:47:21 -0700

    Added bullet configuration, added spec for net_worth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Bullet Gem
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.honeybadger = true
+    Bullet.add_footer = true
+    Bullet.skip_html_injection = false
+  end
 end
