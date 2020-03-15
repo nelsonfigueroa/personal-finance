e8ee23d1213f42aa6591e7e1004d9591dad9f6e7
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-14 23:48:34 -0700

    controller setup

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,18 @@
 class ExpensesController < ApplicationController
+  before_action :assign_user
+  before_action :assign_account, only: %i[create edit update]
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:amount, :date, :expense_tracker_id)
+  end
+
+  def assign_user
+    @user = current_user
+  end
+
+  def assign_expense_tracker
+    @expense_tracker = ExpenseTracker.find_by(id: params[:expense_tracker_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
       resources :statements, only: %i[new create edit update]
     end
 
-    resources :expense_trackers
+    resources :expense_trackers do
+      resources :expenses, only: %i[new create edit update]
+    end
   end
 end
