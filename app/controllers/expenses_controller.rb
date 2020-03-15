2d7eb17007605c2b51e4305124b1387dde4789e3
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-15 00:03:49 -0700

    completed expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,49 @@
 class ExpensesController < ApplicationController
   before_action :assign_user
-  before_action :assign_account, only: %i[create edit update]
+  before_action :assign_expense_tracker, only: %i[create edit update]
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+
+    @expense.expense_tracker_id = @expense_tracker.id
+
+    if @expense.save
+      flash[:notice] = 'Expense created'
+      redirect_to(expense_tracker_path(@expense_tracker))
+    else
+      flash[:alert] = @expense.errors.full_messages.join(', ')
+      render('new')
+    end
+  end
+
+  def edit
+    @expense = @user.expense_trackers.find_by(id: @expense_tracker.id).expenses.find_by(id: params[:id])
+
+    if @expense.nil?
+      flash[:alert] = "Invalid ID"
+      redirect_to(expense_tracker_path(@expense_tracker))
+    end
+  end
+
+  def update
+    @expense = @user.expense_trackers.find_by(id: @expense_tracker.id).expenses.find_by(id: params[:id])
+
+    if @expense.update(expense_params)
+      flash[:notice] = 'Expense updated'
+      redirect_to(expense_tracker_path(@expense_tracker))
+    else
+      flash[:alert] = @expense.errors.full_messages.join(', ')
+      redirect_to(edit_expense_tracker_expense_path(expense_tracker_id: @expense_tracker.id, expense: @expense))
+    end
+  end
+
+  def destroy
+    # expenses should be destroyed when expense tracker is destroyed
+  end
 
   private
 
