e134155cc4b005ea3b0120eb03dc5ddb678e0373
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-14 23:38:18 -0700

    linting

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Expense < ApplicationRecord
   belongs_to :expense_tracker
 
   validates :amount, :date, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0,
-              less_than_or_equal_to: BigDecimal(10**8) }
+                                     less_than_or_equal_to: BigDecimal(10**8) }
 end
