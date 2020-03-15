5f45af016d42799eede76c9e8801327512b756c8
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-14 23:40:03 -0700

    added specs for expense model

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:expense_tracker) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:amount) }
+    it { should validate_presence_of(:date) }
+    it {
+      should validate_numericality_of(:amount)
+        .is_greater_than_or_equal_to(0)
+        .is_less_than_or_equal_to(BigDecimal(10**8))
+    }
+  end
 end
