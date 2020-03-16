e87862d69a2142e6c467f951918508832060acd6
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-03-15 23:58:37 -0700

    added more charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,19 +10,17 @@ class HomeController < ApplicationController
 
     # sum of statements from current month
     @net_worth = @user.statements.where(date: Date.current.beginning_of_month..Date.current.end_of_month).sum(:balance) 
-
     # statements this month - sum of statements from last month
     @net_worth_change = @net_worth - @user.statements.where(date: 1.month.ago.beginning_of_month..1.month.ago.end_of_month).sum(:balance)
-
-    # for chartkick
-    # @net_worth_graph = @user.statements.sorted_by_date.pluck(:date, :balance)
-    # @net_worth_graph = @user.statements.group_by_day(:date).maximum(:balance)
-
     # show net worth month to month
     @net_worth_graph = @user.statements.group_by_month(:date).sum(:balance)
 
-    # pie chart of expenses for the month
-    @expenses = @user.expenses.group(:category).sum(:amount)
+    # total expenses for current month
+    @expenses_this_month = @user.expenses.where(date: Date.current.beginning_of_month..Date.current.end_of_month).sum(:amount) 
+    # expenses by category this month
+    @expenses_by_category = @user.expenses.where(date: Date.current.beginning_of_month..Date.current.end_of_month).group(:category).sum(:amount)
+    # expenses month to month
+    @expenses_month_to_month = @user.expenses.group_by_month(:date).sum(:amount)
 
   end
 end
