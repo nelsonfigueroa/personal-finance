80006e50a10099ed46854d5e92afafba694eb2ca
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-06-07 18:14:12 -0700

    separating API

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,16 +6,11 @@ class AccountsController < ApplicationController
   def index
     @accounts = @user.accounts.order(:name)
 
-    respond_to do |format|
-      format.json { render json: @accounts }
-      format.html do
-        # sum of statements from each account for current month. Maybe this should be for previous month? Becase ending balances might be input at end of month
-        @net_worth = @user.statements.where(date: Time.zone.today.beginning_of_month..Time.zone.today.end_of_month).sum(:balance)
+    # sum of statements from each account for current month. Maybe this should be for previous month? Becase ending balances might be input at end of month
+    @net_worth = @user.statements.where(date: Time.zone.today.beginning_of_month..Time.zone.today.end_of_month).sum(:balance)
 
-        # statements this month - sum of statements from last month
-        @net_worth_change = @net_worth - @user.statements.where(date: Time.zone.today.prev_month.beginning_of_month..Time.zone.today.prev_month.end_of_month).sum(:balance)
-      end
-    end
+    # statements this month - sum of statements from last month
+    @net_worth_change = @net_worth - @user.statements.where(date: Time.zone.today.prev_month.beginning_of_month..Time.zone.today.prev_month.end_of_month).sum(:balance)
   end
 
   def show
