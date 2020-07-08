3cf566059af89ed476479cba2570c385d1d0d63e
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-07-07 19:38:52 -0700

    logging

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 class Api::V1::AccountsController < ApplicationController
-
   def index
-    # no need to specify json, this is configured in routes under :api namespace
-    # render json: User.all
-
-    User.all
+    render json: User.all
   end
 end
