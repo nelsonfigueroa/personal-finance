3b4622d95a3ab55975586db00a947a3149e16b31
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-07-08 01:22:35 -0700

    fixed svg issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # precompile .svgs
+  config.assets.precompile += %w( '.svg' )  
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
