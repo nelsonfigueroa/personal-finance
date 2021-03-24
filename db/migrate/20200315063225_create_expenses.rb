cc654ec1b52ae910de86e16a3991298377c7d31d
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-03-23 23:48:00 -0700

    decimal -> integer

--- db/migrate/20200315063225_create_expenses.rb
+++ db/migrate/20200315063225_create_expenses.rb
@@ -4,7 +4,7 @@ class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
       t.references :expense_tracker, foreign_key: true, index: true
-      t.decimal :amount, precision: 12, scale: 2
+      t.integer :amount
       t.date :date
       t.timestamps
     end
