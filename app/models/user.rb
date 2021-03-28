a56e156bb328f0ec95c3912235db2b9321864e00
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-03-28 02:02:18 -0700

    more cleaning

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
 
   has_many :accounts, dependent: :destroy
   has_many :statements, through: :accounts
-  has_many :expense_trackers, dependent: :destroy
-  has_many :expenses, through: :expense_trackers
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/ } # only letters
