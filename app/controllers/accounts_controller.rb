5331808e3f8bc34c5f98e70249b25f57ce6b811b
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-04-12 03:06:24 -0700

    fixed no account or statement logic

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,15 +4,15 @@ class AccountsController < ApplicationController
   before_action :assign_user
 
   def index
-    @accounts = @user.accounts.order(:name)
+    @accounts = @user.accounts.sorted_by_name
     @net_worth = 0
 
-    @accounts.each do |account|
-      @net_worth += account.statements.sorted_by_date.last.balance_cents
-      puts @net_worth
+    unless @user.statements.empty?
+      @accounts.each do |account|
+        @net_worth += account.statements.sorted_by_date.last.balance_cents
+      end
+      @net_worth /= 100.0
     end
-
-    @net_worth /= 100.0
   end
 
   def show
