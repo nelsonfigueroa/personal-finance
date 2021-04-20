b36cf5e40c5929ae4831e6b5b45232ff60defc6e
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-04-19 20:02:54 -0700

    more logic fixes

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -43,20 +43,13 @@ class ChartsController < ApplicationController
   end
 
   def generate_net_worth_data
-    accounts = @user.accounts
-    statements = @user.statements.sorted_by_date.includes([:account])
+    statements = @user.statements.sorted_by_date
     return nil if statements.empty?
 
     json = {}
 
     earliest_date = statements.first.date
     latest_date = statements.last.date
-
-    # generate the hash
-    account_ending_amounts = {}
-    accounts.each do |account|
-      account_ending_amounts[account.name] = 0
-    end
 
     # loop through each date
     (earliest_date..latest_date).each do |date|
@@ -65,20 +58,8 @@ class ChartsController < ApplicationController
 
       next if statements_in_current_date.empty?
 
-      statements_in_current_date.each do |statement|
-        # update ending amounts hash for each statement that shows up in this date
-        # accounts that don't show up will keep the previous value
-        account_ending_amounts[statement.account.name] = statement.balance_cents / 100.0
-      end
-
-      # sum the values of all accounts and their values in the hash with the current date
-      sum = 0
-      account_ending_amounts.each do |_, value|
-        sum += value
-      end
-
       # add that to json hash that gets returned
-      json[date] = sum
+      json[date] = statements_in_current_date.sum(:balance_cents) / 100.0
 
       # repeat
     end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,11 +4,13 @@ class DashboardController < ApplicationController
   before_action :assign_user
 
   def index
-    @accounts = @user.accounts.sorted_by_name
+    @accounts = @user.accounts.includes([:statements]).sorted_by_name
     @net_worth = 0
 
     unless @user.statements.empty?
+      # get the last statement for each account to determine net worth
       @accounts.each do |account|
+        next if account.statements.empty?
         @net_worth += account.statements.sorted_by_date.last.balance_cents
       end
       @net_worth /= 100.0
