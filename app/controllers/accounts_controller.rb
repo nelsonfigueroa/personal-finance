8502e11aa16a7e270b71aa254f4d7f312a00355a
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-04-19 19:00:57 -0700

    moving views around

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,14 +5,6 @@ class AccountsController < ApplicationController
 
   def index
     @accounts = @user.accounts.sorted_by_name
-    @net_worth = 0
-
-    unless @user.statements.empty?
-      @accounts.each do |account|
-        @net_worth += account.statements.sorted_by_date.last.balance_cents
-      end
-      @net_worth /= 100.0
-    end
   end
 
   def show
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,15 @@ class DashboardController < ApplicationController
   before_action :assign_user
 
   def index
-    @accounts = @user.accounts
+    @accounts = @user.accounts.sorted_by_name
+    @net_worth = 0
+
+    unless @user.statements.empty?
+      @accounts.each do |account|
+        @net_worth += account.statements.sorted_by_date.last.balance_cents
+      end
+      @net_worth /= 100.0
+    end
   end
 
   def assign_user
