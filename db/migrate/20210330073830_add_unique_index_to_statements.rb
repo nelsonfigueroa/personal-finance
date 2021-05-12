fa4c1ee69513849195c5666cee2c7c37059c0add
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2021-05-11 22:43:10 -0700

    simplified migrations

--- db/migrate/20210330073830_add_unique_index_to_statements.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddUniqueIndexToStatements < ActiveRecord::Migration[6.1]
-  def change
-    add_index :statements, %i[account_id date], unique: true
-  end
-end
