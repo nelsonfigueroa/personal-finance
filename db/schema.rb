666fa06826ec5f61d3a354f51e50b68de5f0484d
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-08-17 20:03:08 -0700

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_031348) do
+ActiveRecord::Schema.define(version: 2022_08_18_020015) do
 
   create_table "accounts", force: :cascade do |t|
     t.bigint "user_id"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_03_25_031348) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "statements", force: :cascade do |t|
