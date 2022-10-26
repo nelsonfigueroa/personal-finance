5ec8bc3729f5113e030f5c124e9b331191faad7e
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-10-26 00:37:29 -0700

    scopes for transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,14 @@ class Transaction < ApplicationRecord
 	validates :date, :category, :amount, presence: true
 
 	monetize :amount_cents
+
+	scope :sorted_by_date, -> { order(date: :desc) }
+
+	# https://stackoverflow.com/questions/9624601/activerecord-find-by-year-day-or-month-on-a-date-field
+	def self.by_year(year)
+	  dt = DateTime.new(year)
+	  boy = dt.beginning_of_year
+	  eoy = dt.end_of_year
+	  where("date >= ? and date <= ?", boy, eoy)
+	end
 end
