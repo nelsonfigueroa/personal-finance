381ad08a790f6fbb82b5e99254608d786dff5155
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-02 00:10:00 -0700

    linting of dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -20,36 +20,36 @@ class DashboardController < ApplicationController
 
     ### transactions and spending ###
     @transactions = @user.transactions
-    @yearly_income = @transactions.by_year(Time.now.year).where(category: "Income").sum(:amount_cents) / 100.0
-    @yearly_saved = @transactions.by_year(Time.now.year).where(category: "Savings").sum(:amount_cents) / 100.0
-    @yearly_invested = @transactions.by_year(Time.now.year).where(category: "Investing").sum(:amount_cents) / 100.0
+    @yearly_income = @transactions.by_year(Time.now.year).where(category: 'Income').sum(:amount_cents) / 100.0
+    @yearly_saved = @transactions.by_year(Time.now.year).where(category: 'Savings').sum(:amount_cents) / 100.0
+    @yearly_invested = @transactions.by_year(Time.now.year).where(category: 'Investing').sum(:amount_cents) / 100.0
 
     unless @transactions.empty?
       # @transactions_by_category_per_month = {} # to do
       @transactions_by_category_per_year = {}
 
-      years = @user.transactions.pluck('date').uniq.map { |date| date.year}.uniq
+      years = @user.transactions.pluck('date').uniq.map(&:year).uniq
       # don't include Savings, Investing, and Income categories for expense tracking
-      categories = @user.transactions.where.not(category: ["Savings", "Investing", "Income"]).pluck('category').uniq
+      categories = @user.transactions.where.not(category: %w[Savings Investing Income]).pluck('category').uniq
 
       years.each do |year|
         @transactions_by_category_per_year[year] = {}
         categories.each do |category|
           # sum of transactions by year, by category
-          amount = @user.transactions.by_year(year).where(category: category).sum(:amount_cents) / 100.0
+          amount = @user.transactions.by_year(year).where(category:).sum(:amount_cents) / 100.0
           @transactions_by_category_per_year[year].merge!(category => amount)
         end
 
         # sort hash for each year by values, descending
-        @transactions_by_category_per_year[year] = @transactions_by_category_per_year[year].sort_by {|k, v| -v}.to_h
+        @transactions_by_category_per_year[year] = @transactions_by_category_per_year[year].sort_by { |_k, v| -v }.to_h
       end
 
-    # structure of @transactions_by_category_per_year
-    # {
-    #   "year": {
-    #     "category": "amount"
-    #   }
-    # }
+      # structure of @transactions_by_category_per_year
+      # {
+      #   "year": {
+      #     "category": "amount"
+      #   }
+      # }
     end
   end
 
