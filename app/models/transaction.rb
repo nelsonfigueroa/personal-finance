47edfee22de93ce2757c237f2de7bde1d235d043
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-02 00:11:53 -0700

    proper indentation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class Transaction < ApplicationRecord
-	belongs_to :user
-	validates :date, :category, :amount, presence: true
+  belongs_to :user
+  validates :date, :category, :amount, presence: true
 
-	monetize :amount_cents
+  monetize :amount_cents
 
-	scope :sorted_by_date, -> { order(date: :desc) }
+  scope :sorted_by_date, -> { order(date: :desc) }
 
-	# https://stackoverflow.com/questions/9624601/activerecord-find-by-year-day-or-month-on-a-date-field
-	def self.by_year(year)
-	  dt = DateTime.new(year)
-	  boy = dt.beginning_of_year
-	  eoy = dt.end_of_year
-	  where("date >= ? and date <= ?", boy, eoy)
-	end
+  # https://stackoverflow.com/questions/9624601/activerecord-find-by-year-day-or-month-on-a-date-field
+  def self.by_year(year)
+    dt = DateTime.new(year)
+    boy = dt.beginning_of_year
+    eoy = dt.end_of_year
+    where('date >= ? and date <= ?', boy, eoy)
+  end
 end
