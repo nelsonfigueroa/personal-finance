025105e9c8335ae4d27d6c6b55fb36af1a4432f6
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-09 20:26:13 -0800

    adding some tests for transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:category) }
+    # it { should validate_presence_of(:amount) }
+  end
 end
