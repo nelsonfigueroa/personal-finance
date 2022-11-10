fbdd71f47e8a8d872cd594aca92e9ab80a0369dc
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-09 22:05:05 -0800

    added more tests for transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -10,6 +10,24 @@ RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:date) }
     it { should validate_presence_of(:category) }
-    # it { should validate_presence_of(:amount) }
+
+    # idk how to test for money-rails
+    # it { should validate_presence_of(:amount_cents) }
+
+    it { should validate_numericality_of(:amount_cents) }
+
+    # amount
+    it { should allow_value(Faker::Commerce.price).for(:amount_cents) }
+    it { should_not allow_value(Faker::String.random).for(:amount) }
+
+    # date
+    it { should allow_value(Faker::Date.between(from: 14.days.ago, to: Date.today)).for(:date) }
+    it { should_not allow_value(Faker::String.random).for(:date) }
+
+    # category
+    it { should_not allow_value(nil).for(:category) }
+    it { should allow_value('Testing12345 67890!@#  $%^&*()-_+=.:;/\[]\'",?').for(:category) }
   end
+
+
 end
