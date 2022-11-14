6646a4aa1e7cd89fd04b16e70bebe177046ea852
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-14 00:27:14 -0800

    linting

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "chartkick" # @4.2.0
-pin "Chart.bundle", to: "Chart.bundle.js"
+pin 'application', preload: true
+pin 'chartkick' # @4.2.0
+pin 'Chart.bundle', to: 'Chart.bundle.js'
