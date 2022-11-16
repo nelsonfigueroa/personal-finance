ec220791c78ad806c7f3b9835aec475b9d5a9b12
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-16 01:50:24 -0800

    hotwire installation

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
-pin 'chartkick' # @4.2.0
-pin 'Chart.bundle', to: 'Chart.bundle.js'
+pin "application", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "chartkick" # @4.2.0
+pin 'Chart.bundle', to: 'Chart.bundle.js' # idk if this is even needed
