1b6b497b38ee5b9519c2c7bfea1028fd63d2028d
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-18 16:45:23 -0800

    added seeds for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,34 @@
-# frozen_string_literal: true
+User.create!([
+  {name: "Demo User", email: "demo@demo", password: "demouser123!", encrypted_password: "$2a$11$szyzQvo9zT6uthYP42uUCObOfkOyVr/amilyXYBknFDhHMy8BtyAO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+])
+Account.create!([
+  {user_id: 1, name: "Wells Fargo Debit"},
+  {user_id: 1, name: "Vanguard IRA"},
+  {user_id: 1, name: "Chase Debit"},
+  {user_id: 1, name: "Fidelity Investing"},
+  {user_id: 1, name: "Chase Savings"}
+])
+Statement.create!([
+  {account_id: 3, notes: "", date: "2022-11-01", balance_cents: 230000, balance_currency: "USD"},
+  {account_id: 4, notes: "", date: "2022-11-18", balance_cents: 5014278, balance_currency: "USD"},
+  {account_id: 2, notes: "", date: "2022-11-01", balance_cents: 2092351, balance_currency: "USD"},
+  {account_id: 1, notes: "", date: "2022-11-10", balance_cents: 320000, balance_currency: "USD"},
+  {account_id: 3, notes: "", date: "2022-10-01", balance_cents: 140000, balance_currency: "USD"},
+  {account_id: 1, notes: "", date: "2022-10-01", balance_cents: 100000, balance_currency: "USD"},
+  {account_id: 4, notes: "", date: "2022-10-01", balance_cents: 3850457, balance_currency: "USD"},
+  {account_id: 2, notes: "", date: "2022-10-01", balance_cents: 1470025, balance_currency: "USD"},
+  {account_id: 4, notes: "", date: "2022-11-01", balance_cents: 4520075, balance_currency: "USD"},
+  {account_id: 5, notes: "", date: "2022-11-01", balance_cents: 150000, balance_currency: "USD"},
+  {account_id: 5, notes: "", date: "2022-10-15", balance_cents: 50000, balance_currency: "USD"}
+])
+Transaction.create!([
+  {user_id: 1, date: "2022-11-01", category: "Income", notes: "Monthly Salary", amount_cents: 600000, amount_currency: "USD"},
+  {user_id: 1, date: "2022-10-01", category: "Income", notes: "Monthly Salary", amount_cents: 600000, amount_currency: "USD"},
+  {user_id: 1, date: "2022-10-08", category: "Investing", notes: "$1000 invested.", amount_cents: 100000, amount_currency: "USD"},
+  {user_id: 1, date: "2022-11-01", category: "Savings", notes: "$1000 into Savings", amount_cents: 100000, amount_currency: "USD"},
+  {user_id: 1, date: "2022-11-01", category: "Food", notes: "In n Out", amount_cents: 2235, amount_currency: "USD"},
+  {user_id: 1, date: "2022-11-05", category: "Entertainment", notes: "Movie theater", amount_cents: 3200, amount_currency: "USD"},
+  {user_id: 1, date: "2022-10-10", category: "Gas", notes: "Gas for car.", amount_cents: 6000, amount_currency: "USD"},
+  {user_id: 1, date: "2022-11-06", category: "Groceries", notes: "Costco", amount_cents: 14856, amount_currency: "USD"},
+  {user_id: 1, date: "2022-10-22", category: "Groceries", notes: "Costco", amount_cents: 13245, amount_currency: "USD"}
+])
