e5771f8f8d78f4445692e08fea68d33abba64cbe
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-22 18:07:46 -0800

    reorganized demos

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,14 +3,6 @@
 class ChartsController < ApplicationController
   before_action :assign_user, only: %i[net_worth_graph expenses_pie_chart expenses_column_chart]
 
-  # def expenses_pie_chart_demo
-  #   render json: { 'Clothing' => 0.20454e3, 'Restaurants' => 0.9888e2, 'Entertainment' => 0.5977e2, 'Groceries' => 0.14943e3 }
-  # end
-
-  # def expenses_column_chart_demo
-  #   render json: { 'Jan 2019' => 0.484132e4, 'Feb 2019' => 0.422839e4, 'Mar 2019' => 0.320202e4, 'Apr 2019' => 0.415266e4, 'May 2019' => 0.400948e4 }
-  # end
-
   # net worth
 
   def net_worth_graph
@@ -18,6 +10,16 @@ class ChartsController < ApplicationController
     graph_data = [] if graph_data.nil?
     render json: graph_data
   end
+
+  # demos for future reference 
+
+  # def expenses_pie_chart_demo
+  #   render json: { 'Clothing' => 0.20454e3, 'Restaurants' => 0.9888e2, 'Entertainment' => 0.5977e2, 'Groceries' => 0.14943e3 }
+  # end
+
+  # def expenses_column_chart_demo
+  #   render json: { 'Jan 2019' => 0.484132e4, 'Feb 2019' => 0.422839e4, 'Mar 2019' => 0.320202e4, 'Apr 2019' => 0.415266e4, 'May 2019' => 0.400948e4 }
+  # end
 
   # expense tracking
 
