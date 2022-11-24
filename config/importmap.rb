2236c02b7bd37399e2bfca8ee5d8eb8d4e1e984d
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-11-24 02:25:42 -0800

    linting for importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "chartkick" # @4.2.0
+pin 'application', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin 'chartkick' # @4.2.0
 pin 'Chart.bundle', to: 'Chart.bundle.js' # idk if this is even needed
