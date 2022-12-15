58ef65b91592947a63ccfab68274ca61d6a08cd5
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2022-12-15 01:01:53 -0800

    Aggregating yearly dividends

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,6 +23,7 @@ class DashboardController < ApplicationController
     @yearly_income = @transactions.by_year(Time.zone.now.year).where(category: 'Income').sum(:amount_cents) / 100.0
     @yearly_saved = @transactions.by_year(Time.zone.now.year).where(category: 'Savings').sum(:amount_cents) / 100.0
     @yearly_invested = @transactions.by_year(Time.zone.now.year).where(category: 'Investing').sum(:amount_cents) / 100.0
+    @yearly_dividends = @transactions.by_year(Time.zone.now.year).where(category: 'Dividends').sum(:amount_cents) / 100.0
     @yearly_expenses = @transactions.by_year(Time.zone.now.year).where.not(category: %w[Savings Investing Income]).sum(:amount_cents) / 100.0
 
     return if @transactions.empty?
