30492890ef448547befd6d12f72d5dc91603960f
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-01-15 00:59:30 -0800

    preparing for future tests

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Dashboards', type: :request do
-  describe 'GET /index' do
+  describe 'GET /dashboard' do
+    let(:net_worth) { user.statements.where(date: Time.zone.today.all_month).sum(:balance_cents) }
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
