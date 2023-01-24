bd0dc28aec65587593597d7085c4c5435b58ba99
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-01-23 17:40:06 -0800

    nvm

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -20,7 +20,6 @@ class DashboardController < ApplicationController
 
     ### transactions and spending ###
 
-    # TODO would this be faster if I looped through everything instead of making database queries?
     @transactions = @user.transactions
     @yearly_income = @transactions.by_year(Time.zone.now.year).where(category: 'Income').sum(:amount_cents) / 100.0
     @yearly_saved = @transactions.by_year(Time.zone.now.year).where(category: 'Savings').sum(:amount_cents) / 100.0
