0cc24f0f489966f86777ae8b4a3f47ed9f0fa661
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-01-24 22:34:13 -0800

    no demos

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -10,16 +10,6 @@ class ChartsController < ApplicationController
     render json: graph_data
   end
 
-  # demos for future reference
-
-  # def expenses_pie_chart_demo
-  #   render json: { 'Clothing' => 0.20454e3, 'Restaurants' => 0.9888e2, 'Entertainment' => 0.5977e2, 'Groceries' => 0.14943e3 }
-  # end
-
-  # def expenses_column_chart_demo
-  #   render json: { 'Jan 2019' => 0.484132e4, 'Feb 2019' => 0.422839e4, 'Mar 2019' => 0.320202e4, 'Apr 2019' => 0.415266e4, 'May 2019' => 0.400948e4 }
-  # end
-
   private
 
   def assign_user
