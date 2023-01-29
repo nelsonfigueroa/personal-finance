72afc347950f0062e07f73b460491f117efebc9d
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-01-28 19:40:38 -0800

    new pie chart route

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   authenticated :user do
     get '/dashboard', to: 'dashboard#index'
     get '/net_worth_graph', to: 'charts#net_worth_graph'
-    get '/expenses_pie_chart', to: 'charts#expenses_pie_chart'
-    get '/expenses_column_chart', to: 'charts#expenses_column_chart'
+    get '/pie_chart', to: 'charts#pie_chart'
 
     resources :accounts do
       resources :statements, except: [:show]
