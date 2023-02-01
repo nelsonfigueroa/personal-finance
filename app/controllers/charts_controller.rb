71db6340d1979f6dc324bcf477bc22f942897b3c
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-01-31 20:17:44 -0800

    new pie chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -10,10 +10,15 @@ class ChartsController < ApplicationController
     render json: graph_data
   end
 
-  # pie chart
+  # pie charts
 
   def yearly_expenses_pie_chart
     chart_data = generate_yearly_expenses_chart_data
+    render json: chart_data
+  end
+
+  def yearly_income_pie_chart
+    chart_data = generate_yearly_income_chart_data
     render json: chart_data
   end
 
@@ -23,9 +28,29 @@ class ChartsController < ApplicationController
     @user = current_user
   end
 
+    # then I can figure out how to add titles or labels to the pie chart on the site
+    # and I also need to move over the pie chart to the appropriate section on the view
+
+  def generate_yearly_income_chart_data
+    year = Time.now.year
+    transactions = @user.transactions.by_year(year)
+
+    return {} if transactions.empty?
+
+    categories = transactions.where(category: %w[Savings Investing Income Dividends Interest]).pluck('category').uniq # maybe just hardcode this? will it be faster?
+    data = {}
+
+    categories.each do |category|
+      amount = transactions.where(category: category).sum(:amount_cents) / 100.0
+      data.merge!(category => amount)
+    end
+
+    data
+  end
+
   def generate_yearly_expenses_chart_data
     year = Time.now.year
-    transactions = @user.transactions.by_year(Time.zone.now.year)
+    transactions = @user.transactions.by_year(Time.zone.now.year) # change this to year
 
     return {} if transactions.empty?
 
