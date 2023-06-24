dd73b7024e808eafd7313bea44584b92e6b1621e
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-06-24 00:46:37 -0700

    linting

--- app/controllers/dividends_controller.rb
+++ app/controllers/dividends_controller.rb
@@ -1,82 +1,83 @@
-class DividendsController < ApplicationController
 # frozen_string_literal: true
-    before_action :assign_user
 
-    def index
-        @dividends = @user.dividends
+class DividendsController < ApplicationController
+  # frozen_string_literal: true
+  before_action :assign_user
+
+  def index
+    @dividends = @user.dividends
+  end
+
+  def show
+    @dividend = @user.dividends.find_by(id: params[:id])
+    @dividends = @user.dividends.where(ticker: @dividend.ticker).by_year(Time.zone.now.year)
+    @total_dividends_this_year = @dividends.sum(:amount_cents) / 100.0
+
+    return unless @dividend.nil?
+
+    flash[:alert] = 'Invalid ID'
+    redirect_to(dividends_path)
+  end
+
+  def new
+    @dividend = Dividend.new
+  end
+
+  def edit
+    @dividend = @user.dividends.find_by(id: params[:id])
+
+    return unless @dividend.nil?
+
+    flash[:alert] = 'Invalid ID'
+    redirect_to(dividends_path)
+  end
+
+  def create
+    @dividend = Dividend.new(dividend_params)
+    @dividend.user_id = @user.id
+    if @dividend.save
+      flash[:notice] = 'Dividend created'
+      redirect_to(dividends_path)
+    else
+      flash[:alert] = @dividend.errors.full_messages.join(', ')
+      render('new')
+    end
+  end
+
+  def update
+    @dividend = @user.dividends.find_by(id: params[:id])
+    if @dividend.update(dividend_params)
+      flash[:notice] = 'Dividend updated'
+      redirect_to(dividends_path)
+    else
+      flash[:alert] = @dividend.errors.full_messages.join(', ')
+      redirect_to(edit_dividend_path(@dividend))
+    end
+  end
+
+  def destroy
+    @dividend = @user.dividends.find_by(id: params[:id])
+
+    if @dividend.nil?
+      flash[:alert] = 'Invalid ID'
+      redirect_to(dividends_path) && return
     end
 
-    def show
-        @dividend = @user.dividends.find_by(id: params[:id])
-        @dividends = @user.dividends.where(ticker: @dividend.ticker).by_year(Time.now.year)
-        @total_dividends_this_year = @dividends.sum(:amount_cents) / 100.0
-
-        return unless @dividend.nil?
-
-        flash[:alert] = 'Invalid ID'
-        redirect_to(dividends_path)
+    if @dividend.destroy
+      flash[:notice] = 'Dividend Deleted'
+    else
+      flash[:alert] = @dividend.errors.full_messages.join(', ')
     end
+    redirect_to(dividends_path)
+  end
 
-    def new
-        @dividend = Dividend.new
-    end
+  private
 
-    def edit
-        @dividend = @user.dividends.find_by(id: params[:id])
+  def dividend_params
+    params.require(:dividend).permit(:amount, :date, :ticker)
+  end
 
-        return unless @dividend.nil?
-
-        flash[:alert] = 'Invalid ID'
-        redirect_to(dividends_path)
-    end
-
-    def create
-        @dividend = Dividend.new(dividend_params)
-        @dividend.user_id = @user.id
-        if @dividend.save
-        flash[:notice] = 'Dividend created'
-        redirect_to(dividends_path)
-        else
-        flash[:alert] = @dividend.errors.full_messages.join(', ')
-        render('new')
-        end
-    end
-
-    def update
-        @dividend = @user.dividends.find_by(id: params[:id])
-        if @dividend.update(dividend_params)
-        flash[:notice] = 'Dividend updated'
-        redirect_to(dividends_path)
-        else
-        flash[:alert] = @dividend.errors.full_messages.join(', ')
-        redirect_to(edit_dividend_path(@dividend))
-        end
-    end
-
-    def destroy
-        @dividend = @user.dividends.find_by(id: params[:id])
-
-        if @dividend.nil?
-        flash[:alert] = 'Invalid ID'
-        redirect_to(dividends_path) && return
-        end
-
-        if @dividend.destroy
-        flash[:notice] = 'Dividend Deleted'
-        else
-        flash[:alert] = @dividend.errors.full_messages.join(', ')
-        end
-        redirect_to(dividends_path)
-    end
-
-    private
-
-    def dividend_params
-        params.require(:dividend).permit(:amount, :date, :ticker)
-    end
-
-    def assign_user
-        @user = current_user
-    end
-
+  def assign_user
+    @user = current_user
+  end
 end
