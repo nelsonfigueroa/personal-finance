15e1f045db120a917461a30ea91047802778e7e2
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-06-23 22:37:41 -0700

    linting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   at_exit do
     development_logfile = Rails.application.config.paths['log'].first
 
-    if File.exists?(development_logfile)
+    if File.exist?(development_logfile)
       Rails.logger.debug 'Deleting development.log...'
       File.delete(development_logfile)
     end
