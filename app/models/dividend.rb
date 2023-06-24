cc9cd53e575686746764cda8b7909fed7940e846
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-06-23 22:36:52 -0700

    linting

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class Dividend < ApplicationRecord
-    belongs_to :user
-    validates :date, :amount, :ticker, presence: true
-    validates :amount, numericality: true
-  
-    monetize :amount_cents
-  
-    scope :sorted_by_date, -> { order(date: :desc) }
-  
-    # https://stackoverflow.com/questions/9624601/activerecord-find-by-year-day-or-month-on-a-date-field
-    def self.by_year(year)
-      datetime = DateTime.new(year)
-      beginning_of_year = datetime.beginning_of_year
-      end_of_year = datetime.end_of_year
-      where('date >= ? and date <= ?', beginning_of_year, end_of_year)
-    end
+  belongs_to :user
+  validates :date, :amount, :ticker, presence: true
+  validates :amount, numericality: true
+
+  monetize :amount_cents
+
+  scope :sorted_by_date, -> { order(date: :desc) }
+
+  # https://stackoverflow.com/questions/9624601/activerecord-find-by-year-day-or-month-on-a-date-field
+  def self.by_year(year)
+    datetime = DateTime.new(year)
+    beginning_of_year = datetime.beginning_of_year
+    end_of_year = datetime.end_of_year
+    where('date >= ? and date <= ?', beginning_of_year, end_of_year)
+  end
 end
