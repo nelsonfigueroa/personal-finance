86656fc75bff2a3eed931056c5788cec2afba370
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-08-02 19:06:29 -0700

    user has many categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :statements, through: :accounts
   has_many :transactions, dependent: :destroy
   has_many :dividends, dependent: :destroy
+  has_many :categories, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/ } # only letters
