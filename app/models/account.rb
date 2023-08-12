929a65556195f7d67eecceaa2e82f682674b602f
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-08-11 20:54:59 -0700

    increasing account name length

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,7 +6,7 @@ class Account < ApplicationRecord
   has_many :statements, dependent: :destroy
 
   validates :user, :name, presence: true
-  validates :name, length: { maximum: 40 }
+  validates :name, length: { maximum: 100 }
 
   scope :sorted_by_name, -> { order(name: :asc) }
 end
