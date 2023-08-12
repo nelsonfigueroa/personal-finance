877d7fa150feff9de2996b59465d3a2faefaad34
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-08-11 20:52:39 -0700

    more automated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,35 @@
 # frozen_string_literal: true
 
+require 'faker'
+
 User.create!([
                { name: 'Demo User', email: 'demo@demo', password: 'demouser123!', encrypted_password: '$2a$11$szyzQvo9zT6uthYP42uUCObOfkOyVr/amilyXYBknFDhHMy8BtyAO', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil }
              ])
-Account.create!([
-                  { user_id: 1, name: 'Wells Fargo Debit' },
-                  { user_id: 1, name: 'Vanguard IRA' },
-                  { user_id: 1, name: 'Chase Debit' },
-                  { user_id: 1, name: 'Fidelity Investing' },
-                  { user_id: 1, name: 'Chase Savings' }
-                ])
-Statement.create!([
-                    { account_id: 3, notes: '', date: '2023-02-01', balance_cents: 230_000, balance_currency: 'USD' },
-                    { account_id: 4, notes: '', date: '2023-02-18', balance_cents: 5_014_278, balance_currency: 'USD' },
-                    { account_id: 2, notes: '', date: '2023-02-01', balance_cents: 2_092_351, balance_currency: 'USD' },
-                    { account_id: 1, notes: '', date: '2023-02-10', balance_cents: 320_000, balance_currency: 'USD' },
-                    { account_id: 3, notes: '', date: '2023-01-01', balance_cents: 140_000, balance_currency: 'USD' },
-                    { account_id: 1, notes: '', date: '2023-01-01', balance_cents: 100_000, balance_currency: 'USD' },
-                    { account_id: 4, notes: '', date: '2023-01-01', balance_cents: 3_850_457, balance_currency: 'USD' },
-                    { account_id: 2, notes: '', date: '2023-01-01', balance_cents: 1_470_025, balance_currency: 'USD' },
-                    { account_id: 4, notes: '', date: '2023-02-01', balance_cents: 4_520_075, balance_currency: 'USD' },
-                    { account_id: 5, notes: '', date: '2023-02-01', balance_cents: 150_000, balance_currency: 'USD' },
-                    { account_id: 5, notes: '', date: '2023-01-15', balance_cents: 50_000, balance_currency: 'USD' }
-                  ])
-Transaction.create!([
-                      { user_id: 1, date: '2023-02-01', category: 'Income', notes: 'Monthly Salary', amount_cents: 600_000, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-01-01', category: 'Income', notes: 'Monthly Salary', amount_cents: 600_000, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-01-08', category: 'Investing', notes: '$1000 invested.', amount_cents: 100_000, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-02-01', category: 'Savings', notes: '$1000 into Savings', amount_cents: 100_000, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-02-01', category: 'Food', notes: 'In n Out', amount_cents: 2235, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-02-05', category: 'Entertainment', notes: 'Movie theater', amount_cents: 3200, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-01-10', category: 'Gas', notes: 'Gas for car.', amount_cents: 6000, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-02-06', category: 'Groceries', notes: 'Costco', amount_cents: 14_856, amount_currency: 'USD' },
-                      { user_id: 1, date: '2023-01-22', category: 'Groceries', notes: 'Costco', amount_cents: 13_245, amount_currency: 'USD' }
-                    ])
+
+# create 5 accounts for the user
+5.times do
+  Account.create!(user_id: 1, name: Faker::Bank.unique.name)
+end
+
+# each account gets 24 statements to cover a 2 year span
+24.times do |i|
+  i += 1
+  date = Faker::Date.between(from: (25 - i).months.ago, to: (24 - i).months.ago)
+
+  # for each account
+  5.times do |x|
+    x += 1
+    Statement.create!(account_id: x, notes: 'notes', date: date, balance_cents: Faker::Number.between(from: (i * 100000), to: ((i + 1) * 150000)), balance_currency: 'USD')
+  end
+end
+
+Category.create!([
+  { user_id: 1, name: "Income", color: "#1cfc03" }
+  { user_id: 1, name: "Interest", color: "#1cfc03" },
+  { user_id: 1, name: "Rent", color: "#FFFFFF" },
+  { user_id: 1, name: "Groceries", color: "#00bfff" },
+  { user_id: 1, name: "Restaurants", color: "#ff0022" },
+  { user_id: 1, name: "Car", color: "#ff0022" },
+  { user_id: 1, name: "Shopping", color: "#eeff00" },
+  { user_id: 1, name: "Games", color: "#03a5fc" },
+])
