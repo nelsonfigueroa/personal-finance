f810324483d2c549bd6e9d24963b887d2c756562
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-08-22 19:15:43 -0700

    uniqueness constraint between category name and user ID

---
+++ db/migrate/20230822233918_add_uniqueness_between_category_name_and_user_id.rb
@@ -0,0 +1,7 @@
+class AddUniquenessBetweenCategoryNameAndUserId < ActiveRecord::Migration[7.0]
+  def change
+    change_table :categories do |t|
+      t.index %i[name user_id], unique: true # category name and account_id are unique together
+    end
+  end
+end
