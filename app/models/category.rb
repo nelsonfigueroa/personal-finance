1c72fadb64f4f4ea2b3481ce720ccf217da61a10
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-08-24 20:59:17 -0700

    more validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :transactions
 
+    validates :name, presence: true
     validates :color, presence: true, format: { with: /\A#?(?:[0-9a-fA-F]{3}){1,2}\z/, message: "must be a valid hex color" }
+    validates :name, uniqueness: { scope: :user_id, message: 'you already have a category with this name' }
 end
