c969c173e56004beb8e6841840464c4cfe41a9b5
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2023-09-26 18:38:46 -0700

    linting

--- db/migrate/20230803014913_add_category_reference_to_transactions.rb
+++ db/migrate/20230803014913_add_category_reference_to_transactions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCategoryReferenceToTransactions < ActiveRecord::Migration[7.0]
   def change
     add_reference :transactions, :category, null: true, foreign_key: true
--- db/migrate/20230830032906_add_merchant_to_transactions.rb
+++ db/migrate/20230830032906_add_merchant_to_transactions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMerchantToTransactions < ActiveRecord::Migration[7.0]
   def change
     add_column :transactions, :merchant, :string
