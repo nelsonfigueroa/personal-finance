1e79715953a9a8495f50486b3ed612cd56224c83
nelsonfigueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2024-02-07 11:11:17 -0800

    linting

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,26 +19,26 @@ end
   # for each account
   5.times do |x|
     x += 1
-    Statement.create!(account_id: x, notes: 'notes', date: date, balance_cents: Faker::Number.between(from: (i * 100000), to: ((i + 1) * 150000)), balance_currency: 'USD')
+    Statement.create!(account_id: x, notes: 'notes', date: date, balance_cents: Faker::Number.between(from: (i * 100_000), to: ((i + 1) * 150_000)), balance_currency: 'USD')
   end
 end
 
 Category.create!([
-  { user_id: 1, name: "Income", color: "#1cfc03" },
-  { user_id: 1, name: "Interest", color: "#1cfc03" },
-  { user_id: 1, name: "Rent", color: "#FFFFFF" },
-  { user_id: 1, name: "Groceries", color: "#00bfff" },
-  { user_id: 1, name: "Restaurants", color: "#ff0022" },
-  { user_id: 1, name: "Car", color: "#ff0022" },
-  { user_id: 1, name: "Shopping", color: "#eeff00" },
-  { user_id: 1, name: "Games", color: "#03a5fc" }
-])
+                   { user_id: 1, name: 'Income', color: '#1cfc03' },
+                   { user_id: 1, name: 'Interest', color: '#1cfc03' },
+                   { user_id: 1, name: 'Rent', color: '#FFFFFF' },
+                   { user_id: 1, name: 'Groceries', color: '#00bfff' },
+                   { user_id: 1, name: 'Restaurants', color: '#ff0022' },
+                   { user_id: 1, name: 'Car', color: '#ff0022' },
+                   { user_id: 1, name: 'Shopping', color: '#eeff00' },
+                   { user_id: 1, name: 'Games', color: '#03a5fc' }
+                 ])
 
 # Income for 4 year span
 48.times do |i|
   i += 1
   date = Faker::Date.between(from: (49 - i).months.ago, to: (48 - i).months.ago)
-  Transaction.create!(user_id: 1, date: date, category_id: 1, notes: 'Salary', amount_cents: 400000, amount_currency: 'USD')
+  Transaction.create!(user_id: 1, date: date, category_id: 1, notes: 'Salary', amount_cents: 400_000, amount_currency: 'USD')
 end
 
 # Interest Earnings for 4 year span
@@ -54,9 +54,9 @@ current_date = Date.current
 4.times do
   # 12 months
   12.times do
-      Transaction.create!(user_id: 1, date: current_date, category_id: 3, notes: 'Rent', amount_cents: 200000, amount_currency: 'USD')
-      # Move to previous month
-      current_date = current_date.prev_month
+    Transaction.create!(user_id: 1, date: current_date, category_id: 3, notes: 'Rent', amount_cents: 200_000, amount_currency: 'USD')
+    # Move to previous month
+    current_date = current_date.prev_month
   end
 
   # Move to the previous year
@@ -67,7 +67,7 @@ end
 48.times do |i|
   i += 1
   date = Faker::Date.between(from: (49 - i).months.ago, to: (48 - i).months.ago)
-  Transaction.create!(user_id: 1, date: date, category_id: 4, notes: 'Groceries', amount_cents: 100000, amount_currency: 'USD')
+  Transaction.create!(user_id: 1, date: date, category_id: 4, notes: 'Groceries', amount_cents: 100_000, amount_currency: 'USD')
 end
 
 # Restaurants for 4 year span
